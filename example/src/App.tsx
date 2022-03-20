0b4a1abb0dea7847214c10b6a49e0c2a18b73520
jiggag <jiggag90@gmail.com>
2022-03-20 13:31:49 +0900

    feat: 커스텀 마커 이미지 예시

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,6 +5,35 @@ import RnKakaoMaps from '@jiggag/react-native-kakao-maps';
 export function App() {
   const onPress = useCallback(() => {
     RnKakaoMaps.show({
+      markerList: [],
+    });
+  }, []);
+
+  const onPressImageName = useCallback(() => {
+    RnKakaoMaps.show({
+      markerImageName: 'custom_image',
+      markerList: [
+        {
+          markerName: 'Marker1',
+          lat: 37.5359,
+          lng: 127.005518,
+        },
+        {
+          markerName: 'Marker2',
+          lat: 37.5373,
+          lng: 127.005515,
+        },
+      ],
+      centerPoint: {
+        lat: 37.5359,
+        lng: 127.005518,
+      },
+    });
+  }, []);
+
+  const onPressImageUrl = useCallback(() => {
+    RnKakaoMaps.show({
+      markerImageUrl: 'https://github.com/jiggag/react-native-kakao-maps/tree/develop/example/custom_image.png',
       markerList: [
         {
           markerName: 'Marker1',
@@ -28,6 +57,8 @@ export function App() {
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <Button title="Show" onPress={onPress} />
+        <Button title="Show Custom (markerImageName)" onPress={onPressImageName} />
+        <Button title="Show Custom (markerImageUrl)" onPress={onPressImageUrl} />
       </ScrollView>
     </SafeAreaView>
   );
