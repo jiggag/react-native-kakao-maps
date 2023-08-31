bec8e1c68d6bba00f98c83776314d6c3e7d9d628
jiggag <jiggag90@gmail.com>
2023-08-31 22:02:41 +0900

    리렌더 테스트를 위한 인터벌 업데이트

--- example/App.js
+++ example/App.js
@@ -1,33 +1,58 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { KakaoMapView } from '@jiggag/react-native-kakao-maps';
+import React, {useCallback, useEffect, useState} from 'react';
+import {SafeAreaView} from 'react-native';
+import {KakaoMapView} from '@jiggag/react-native-kakao-maps';
+
+const Constants = {
+  CENTER_POINT: {
+    lat: 37.59523,
+    lng: 127.086,
+  },
+  MARKER_LIST: [
+    {
+      lat: 37.59523,
+      lng: 127.086,
+      markerName: 'marker',
+    },
+    {
+      lat: 37.59523,
+      lng: 127.08705,
+      markerName: 'marker2',
+    },
+  ],
+};
 
 const App = () => {
+  const [markerList, setMarkerList] = useState([]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setMarkerList(prevState => {
+        if (prevState.length === 2) {
+          return [];
+        }
+
+        return [...prevState, Constants.MARKER_LIST[prevState.length % 2]];
+      });
+    }, 2000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  const onChange = useCallback(event => {
+    console.log('[onChange]', event.nativeEvent);
+  }, []);
+
   return (
     <SafeAreaView>
       <KakaoMapView
         markerImageUrl="https://github.com/jiggag/react-native-kakao-maps/blob/develop/example/custom_image.png?raw=true"
-        markerList={[
-          {
-            lat: 37.59523,
-            lng: 127.08600,
-            markerName: 'marker'
-          },
-          {
-            lat: 37.59523,
-            lng: 127.08705,
-            markerName: 'marker2'
-          },
-        ]}
+        markerList={markerList}
         width={300}
         height={500}
-        centerPoint={{
-          lat: 37.59523,
-          lng: 127.08600,
-        }}
-        onChange={(event) => {
-          console.log('[onChange]', event.nativeEvent);
-        }}
+        centerPoint={Constants.CENTER_POINT}
+        onChange={onChange}
       />
     </SafeAreaView>
   );
